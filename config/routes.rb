9ae9d7ecc88f1ed0056f2984dd358d5a9d684aad
Hiro <ishikawa.hiroyuki1118@gmail.com>
2018-02-27 14:03:38 +0000

    Checkpoint 22 CRUD: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
-  
-  get 'welcome/contact'
-
-  get 'welcome/faq'
+  resources :posts
+  get 'about' => "welcome#about"
 
   root 'welcome#index'
 
